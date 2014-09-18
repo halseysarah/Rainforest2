bcfa36fb7f9a708116384e16aa578bd50a554dde
SARAH <HALSEYSARAH@Gmail.com>
2014-09-18 12:41:59 -0400

    Refactored $.ajax() to $.get()

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,25 +1,12 @@
 $(document).on('ready page:load', function() {
-
-<script type="text/javascript">
-  function display_search_results() {
-    // readyState === 4 means that the asynchronous request completed successfully
-    if (this.readyState === 4) {
-      console.log(this);
-      document.getElementById('products').innerHTML = this.responseText;
-    }
-  }
-
-  var form = document.getElementById('search-form');
-  form.addEventListener('submit', function(event) {
+  $('#search-form').submit(function(event) {
     event.preventDefault();
-    var searchValue = document.getElementById('search').value;
+    var searchValue = $('#search').val();
 
-    var xhr = new XMLHttpRequest();
-    xhr.onload = display_search_results;
-    xhr.open('GET', '/products?search=' + searchValue, true);
-    xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-    xhr.send();
+    $.get('/products?search=' + searchValue)
+      .done(function(data){
+        console.log(data);
+        $('#products').html(data);
+      });
   });
-</script>
-
 });
